f45c89bbb68462b922241eadf1af989e5566b8cb
Jay Jamison <jeremiah.s.jamison@gmail.com>
2013-04-01 22:37:12 -0700

    working basic authorization on edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,    only: [:edit, :update]
 
   def show
   	@user = User.find(params[:id])
@@ -20,7 +22,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -35,4 +36,14 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+    def signed_in_user
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_path unless current_user?(@user)
+    end
+  
 end
