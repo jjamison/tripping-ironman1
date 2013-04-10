16b8c66ed2a1ea3676abf9702ac3778fb897b1dc
Jay Jamison <jeremiah.s.jamison@gmail.com>
2013-04-10 11:39:45 -0700

    working followed users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed 
 
   before_save { |user| user.email = user.email.downcase }
   before_save :create_remember_token 
@@ -13,6 +14,19 @@ class User < ActiveRecord::Base
   					uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)  
+    relationships.find_by_followed_id(other_user.id).destroy  
+  end
 
   def feed
     Micropost.where("user_id = ?", id)
